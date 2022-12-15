6080b2c5346ff1668e73b7638275d8dc96a9c299
tech1ndex <50547519+tech1ndex@users.noreply.github.com>
2022-12-14 19:15:49 -0500

    removed k3s-fw

--- k3s-master.tf
+++ k3s-master.tf
@@ -56,3 +56,16 @@ resource "google_compute_instance" "k3s_master_instance" {
   ]
 }
 
+resource "google_compute_firewall" "k3s-firewall" {
+  name    = "k3s-firewall"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["6443"]
+  }
+
+  target_tags = ["k3s"]
+  source_ranges = [var.publicip]
+  
+}
